719736cbc8c2f55d0a15b8c857b9d3926151c60a
John Tobin <tobinjt@cs.tcd.ie>
2010-02-01 17:28:20 +0000

    Project Euler 6

--- project-euler.lisp
+++ project-euler.lisp
@@ -271,3 +271,33 @@
   )
   an-array
 ); }}}
+
+;The sum of the squares of the first ten natural numbers is, 1^(2) + 2^(2) + ...
+;+ 10^(2) = 385
+;
+;The square of the sum of the first ten natural numbers is, (1 + 2 + ... +
+;10)^(2) = 55^(2) = 3025
+;
+;Hence the difference between the sum of the squares of the first ten natural
+;numbers and the square of the sum is 3025 − 385 = 2640.
+;
+;Find the difference between the sum of the squares of the first one hundred
+;natural numbers and the square of the sum.
+
+(defun project-euler-6-1 (); {{{
+  (let*
+    (
+      (n 100)
+
+      (sum-n (/ (* n (1+ n)) 2))
+      (square-sum-n (expt sum-n 2))
+
+      ; n(n + 1)(2n + 1)/6
+      ; http://en.wikipedia.org/wiki/Sum
+      (sum-squares (/ (* n (1+ n) (1+ (* n 2))) 6))
+    )
+
+    (- square-sum-n sum-squares)
+  )
+
+); }}}
