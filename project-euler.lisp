2f33c6c023448fd685a4f827a994db9f205f3ab6
John Tobin <tobinjt@cs.tcd.ie>
2010-03-27 20:12:24 +0000

    Project Euler 25

--- project-euler.lisp
+++ project-euler.lisp
@@ -1376,3 +1376,38 @@
         (when (not sum-found)
           (incf sum a-number))))
     sum)); }}}
+
+; The Fibonacci sequence is defined by the recurrence relation:
+;     Fn = Fn-1 + Fn-2, where F1 = 1 and F2 = 1.
+; Hence the first 12 terms will be:
+;
+; F1 = 1
+; F2 = 1
+; F3 = 2
+; F4 = 3
+; F5 = 5
+; F6 = 8
+; F7 = 13
+; F8 = 21
+; F9 = 34
+; F10 = 55
+; F11 = 89
+; F12 = 144
+; The 12th term, F12, is the first term to contain three digits.
+;
+; What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+(defun project-euler-25-1 ()
+  (let ((fib-2 1)
+        (fib-1 1)
+        (fib 0)
+        ; The first loop iteration will caclulate F3.
+        (num-terms 2)
+        (greater-than (expt 10 999)))
+    (loop while (< fib greater-than) do
+      (setf fib (+ fib-1 fib-2)
+            fib-2 fib-1
+            fib-1 fib
+            num-terms (1+ num-terms)))
+    num-terms)
+)
