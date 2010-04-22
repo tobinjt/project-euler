2bf8b0d47dddac7fb61a31ebe3f136f12efaaaca
John Tobin <tobinjt@cs.tcd.ie>
2010-04-22 17:41:38 +0100

    Porject Euler 28

--- project-euler.lisp
+++ project-euler.lisp
@@ -1632,3 +1632,42 @@
                     best-b b
                     best-length num-primes))))))
     (list best-a best-b best-length (* best-a best-b)))); }}}
+
+; Starting with the number 1 and moving to the right in a clockwise direction a
+; 5 by 5 spiral is formed as follows:
+;
+; 21 22 23 24 25
+; 20  7  8  9 10
+; 19  6  1  2 11
+; 18  5  4  3 12
+; 17 16 15 14 13
+;
+; It can be verified that the sum of the numbers on the diagonals is 101.
+;
+; What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+; formed in the same way?
+
+; 43 44 45 46 47 48 49
+; 42 21 22 23 24 25 26
+; 41 20  7  8  9 10 27
+; 40 19  6  1  2 11 28
+; 39 18  5  4  3 12 29
+; 38 17 16 15 14 13 30
+; 37 36 35 34 33 32 31
+
+(defun project-euler-28-1 (); {{{
+  ;current = current + 10 * increment
+  (do ((total 1)
+       (current 1)
+       (increment 2 (+ 2 increment)))
+      ((>= current (* 1001 1001)) total)
+    ; Each time you travel round the square, total increases by:
+    ;     current +  1 * increment
+    ;     current +  2 * increment
+    ;     current +  3 * increment
+    ;     current +  4 * increment
+    ; ---------------------
+    ; 4 * current + 10 * increment
+    ; current increases by 4 * increment
+    (incf total   (+ (* 4 current) (* 10 increment)))
+    (incf current (* 4 increment)))); }}}
