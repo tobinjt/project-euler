ace6a45a0e33292101415d1bce9f73cc53273857
John Tobin <tobinjt@cs.tcd.ie>
2010-12-25 13:33:13 +0000

    Project Euler 50 description.

--- project-euler.lisp
+++ project-euler.lisp
@@ -2420,3 +2420,18 @@
                                                        (- (second possible-answer) (third possible-answer)))))
                                            list-of-possible-answers))
                         list-of-lists-of-possible-answers))))); }}}
+
+; The prime 41, can be written as the sum of six consecutive primes:; {{{
+;
+; 41 = 2 + 3 + 5 + 7 + 11 + 13
+; This is the longest sum of consecutive primes that adds to a prime below
+; one-hundred.
+;
+; The longest sum of consecutive primes below one-thousand that adds to a prime,
+; contains 21 terms, and is equal to 953.
+;
+; Which prime, below one-million, can be written as the sum of the most
+; consecutive primes?; }}}
+
+(defun project-euler-50-1 (); {{{
+  ); }}}
