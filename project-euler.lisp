40cd4f351d45791fb5bc62ae7bfa864c81eea0fc
John Tobin <tobinjt@cs.tcd.ie>
2011-06-29 22:40:10 +0100

    Project Euler 56.

--- project-euler.lisp
+++ project-euler.lisp
@@ -3031,3 +3031,19 @@
     (dofromto (1 9999 current-number num-lychrel-numbers)
       (when (is-lychrel-number current-number)
         (incf num-lychrel-numbers))))); }}}
+
+; A googol (10^100) is a massive number: one followed by one-hundred zeros;; {{{
+; 100^100 is almost unimaginably large: one followed by two-hundred zeros.
+; Despite their size, the sum of the digits in each number is only 1.
+;
+; Considering natural numbers of the form, a^b, where a, b < 100, what is the
+; maximum digital sum?; }}}
+
+(defun project-euler-56-1 (); {{{
+  (let ((highest-sum 0))
+    (dofromto (1 99 a highest-sum)
+      (let ((a^b a))
+        (dofromto (2 99 b)
+          (setf a^b (* a^b a))
+          (setf highest-sum (max highest-sum
+                                 (apply #'+ (number-to-digits a^b))))))))); }}}
