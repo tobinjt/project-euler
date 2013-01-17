988fe44e4ee23a193db5f57238bf92b9c5b01ad1
John Tobin <johntobin@johntobin.ie>
2013-01-17 21:34:42 +0000

    Data structures, stringification, and a constructor for Project Euler 68.

--- project-euler.go
+++ project-euler.go
@@ -87,6 +87,98 @@ func projectEuler67() {
 	fmt.Println(triangle[0][0])
 }
 
+/*
+* Consider the following "magic" 3-gon ring, filled with the numbers 1 to 6, and
+* each line adding to nine.
+*
+*
+* Working clockwise, and starting from the group of three with the numerically
+* lowest external node (4,3,2 in this example), each solution can be described
+* uniquely. For example, the above solution can be described by the set: 4,3,2;
+* 6,2,1; 5,1,3.
+*
+* It is possible to complete the ring with four different totals: 9, 10, 11, and
+* 12. There are eight solutions in total.
+*
+* Total	Solution Set
+* 9	4,2,3; 5,3,1; 6,1,2
+* 9	4,3,2; 6,2,1; 5,1,3
+* 10	2,3,5; 4,5,1; 6,1,3
+* 10	2,5,3; 6,3,1; 4,1,5
+* 11	1,4,6; 3,6,2; 5,2,4
+* 11	1,6,4; 5,4,2; 3,2,6
+* 12	1,5,6; 2,6,4; 3,4,5
+* 12	1,6,5; 3,5,4; 2,4,6
+* By concatenating each group it is possible to form 9-digit strings; the
+* maximum string for a 3-gon ring is 432621513.
+*
+* Using the numbers 1 to 10, and depending on arrangements, it is possible to
+* form 16- and 17-digit strings. What is the maximum 16-digit string for a
+* "magic" 5-gon ring?
+*/
+/*
+* The images are missing, so here's a description: an X-gon ring has X/2 nodes
+* in a ring, and X/2 nodes hanging off the ring.  E.g. a 3-gon looks like:
+* O--O   O   A--B   D
+*   / \ /      / \ /
+*  O---O      E---C
+*   \          \
+*    O          F
+* It's read as: A,B,C; D,C,E; F,E,B - you start with the lowest external node.
+*/
+
+type NGonOuter struct {
+	value int
+	inner *NGonInner
+}
+type NGonInner struct {
+	value int
+	inner *NGonInner
+	outer *NGonOuter
+}
+type NGon struct {
+	inners []NGonInner
+	outers []NGonOuter
+}
+
+func (inner NGonInner) String() string {
+	return fmt.Sprintf("%v, %v, %v", inner.outer.value, inner.value,
+		inner.inner.value)
+}
+
+func (gon *NGon) String() string {
+	results := make([]string, 0)
+	first, value := 0, 0
+	for i := range gon.inners {
+		if gon.inners[i].outer.value > value {
+			first = i
+			value = gon.inners[i].outer.value
+		}
+	}
+	for i := range gon.inners {
+		inner := gon.inners[(first + i) % len(gon.inners)]
+		results = append(results, fmt.Sprint(inner))
+	}
+	return fmt.Sprintf("%v: ", len(gon.inners)) +
+		strings.Join(results, "; ")
+}
+
+func NewNGon(n int) *NGon {
+	gon := &NGon{
+		inners: make([]NGonInner, n),
+		outers: make([]NGonOuter, n),
+	}
+	for i := range gon.inners {
+		gon.inners[i].outer = &gon.outers[i]
+		gon.inners[i].inner = &gon.inners[(i + 1) % n]
+		gon.outers[i].inner = &gon.inners[i]
+	}
+	return gon
+}
+
 func main() {
-	projectEuler67()
+	gon := NewNGon(3)
+	gon.inners[1].outer.value = 1
+	gon.inners[2].outer.value = 2
+	fmt.Printf("%+v\n", gon)
 }
