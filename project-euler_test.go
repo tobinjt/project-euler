a82c680bc482dbb9c9c122b0733b2c130a01f215
John Tobin <johntobin@johntobin.ie>
2013-02-21 18:49:46 +0000

    Better initialisation.

--- project-euler_test.go
+++ project-euler_test.go
@@ -16,10 +16,10 @@ func TestParseTriangle(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expected := make([][]int, 2)
-	expected[0] = []int{1, 0}
-	expected[1] = []int{2, 4}
-	fmt.Println(expected)
+	expected := [][]int{
+		[]int{1},
+		[]int{2, 3},
+	}
 	if !reflect.DeepEqual(expected, triangle) {
 		t.Errorf("%#v != %#v\n", expected, triangle)
 	}
