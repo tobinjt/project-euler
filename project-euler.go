5e5c79eb03984e52511d7584a6da1cf008d0bdc9
John Tobin <johntobin@johntobin.ie>
2013-02-24 23:00:20 +0000

    Add PE 70 problem and links.

--- project-euler.go
+++ project-euler.go
@@ -569,6 +569,28 @@ func projectEuler69() int64 {
 	return int64(result)
 }
 
+/*
+* Euler's Totient function, φ(n) [sometimes called the phi function], is used to
+* determine the number of positive numbers less than or equal to n which are
+* relatively prime to n. For example, as 1, 2, 4, 5, 7, and 8, are all less than
+* nine and relatively prime to nine, φ(9)=6.
+* The number 1 is considered to be relatively prime to every positive number, so
+* φ(1)=1.
+*
+* Interestingly, φ(87109)=79180, and it can be seen that 87109 is a permutation
+* of 79180.
+*
+* Find the value of n, 1 <= n <= 10**7, for which φ(n) is a permutation of n and
+* the ratio n/φ(n) produces a minimum.
+ */
+/*
+* http://www.doc.ic.ac.uk/~mrh/330tutor/ch05s02.html
+* - when n is a prime number (e.g. 2, 3, 5, 7, 11, 13), φ(n) = n-1.
+* - when m and n are coprime, φ(m*n) = φ(m)*φ(n).
+* - If the prime factorisation of n is given by n =p1e1*...*pnen, then
+*   φ(n) = n *(1 - 1/p1)* ... (1 - 1/pn).
+*/
+
 func main() {
 	functions := map[string]func() int64{
 		"67": projectEuler67,
