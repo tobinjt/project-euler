8f4a7ec7f184e5056a776230779858732c272dc7
John Tobin <johntobin@johntobin.ie>
2013-03-07 08:54:39 +0000

    Change tests to use assert.  Improve TestPrimeFactors.

--- project-euler_test.go
+++ project-euler_test.go
@@ -4,7 +4,6 @@ import (
 	"github.com/tobinjt/assert"
 	"bytes"
 	"fmt"
-	"reflect"
 	"sort"
 	"testing"
 )
@@ -20,26 +19,23 @@ func TestParseTriangle(t *testing.T) {
 	fh := bytes.NewBufferString("1\n2 3")
 	triangle, err := parseTriangle(fh)
 	if err == nil {
-		t.Fatal("missing newline should have failed")
+		t.Error("missing newline should have failed")
 	}
 
 	fh = bytes.NewBufferString("x\n")
 	triangle, err = parseTriangle(fh)
 	if err == nil {
-		t.Fatal("non-number should have failed")
+		t.Error("non-number should have failed")
 	}
 
 	fh = bytes.NewBufferString("1\n2 3\n")
 	triangle, err = parseTriangle(fh)
-	if err != nil {
-		t.Fatal(err)
-	}
-	expected := [][]int{
-		[]int{1},
-		[]int{2, 3},
-	}
-	if !reflect.DeepEqual(expected, triangle) {
-		t.Errorf("%#v != %#v\n", expected, triangle)
+	if assert.ErrIsNil(t, "parseTriangle()", err) {
+		expected := [][]int{
+			[]int{1},
+			[]int{2, 3},
+		}
+		assert.Equal(t, "parseTriangle()", expected, triangle)
 	}
 }
 
@@ -48,27 +44,13 @@ func TestNgons(t *testing.T) {
 	gon.Set(0, []int{2, 5, 8})
 	gon.Set(1, []int{1, 6, 7})
 	gon.Set(2, []int{3, 4, 9})
-	triple := gon.Get(0)
-	expected_triple := []int{2, 9, 6}
-	if !reflect.DeepEqual(expected_triple, triple) {
-		t.Errorf("%#v != %#v\n", expected_triple, triple)
-	}
-	str := gon.String()
-	if str != "sum: 11: first: 1 1,6,4; 3,4,9; 2,9,6" {
-		t.Error("bad string:", str)
-	}
-	gon_copy := gon.Copy()
-	if !reflect.DeepEqual(gon, gon_copy) {
-		t.Errorf("copy != original: %#v != %#v\n", gon, gon_copy)
-	}
+	assert.Equal(t, "gon.Get()", []int{2, 9, 6}, gon.Get(0))
+	assert.Equal(t, "gon.String()", "sum: 11: first: 1 1,6,4; 3,4,9; 2,9,6",
+		gon.String())
+	assert.Equal(t, "gon.Copy()", gon, gon.Copy())
 	actual_int, err := gon.ToInt()
-	expected_int := int64(164349296)
-	if err != nil {
-		t.Error(err)
-	}
-	if actual_int != expected_int {
-		t.Errorf("ToInt: %v != %v\n", expected_int, actual_int)
-	}
+	assert.ErrIsNil(t, "gon.ToInt()", err)
+	assert.Equal(t, "gon.ToInt()", int64(164349296), actual_int)
 }
 
 func TestPermutable(t *testing.T) {
@@ -82,28 +64,20 @@ func TestPermutable(t *testing.T) {
 		[]int{3, 1},
 		[]int{3, 2},
 	}
-	if !reflect.DeepEqual(expected_perm, perm.dest) {
-		t.Errorf("bad Permute() result: %#v != %#v\n", expected_perm,
-			perm.dest)
-	}
+	assert.Equal(t, "Permute()", expected_perm, perm.dest)
 }
 
 func TestInt64Sort(t *testing.T) {
 	sort_me := Int64Slice{6, 3, 8, 1}
 	sort.Sort(sort_me)
-	sorted := Int64Slice{1, 3, 6, 8}
-	if !reflect.DeepEqual(sort_me, sorted) {
-		t.Errorf("sort failed: %#v != %#v\n", sort_me, sorted)
-	}
+	assert.Equal(t, "Sort()", Int64Slice{1, 3, 6, 8}, sort_me)
 }
 
 func TestSieveOfEratosthenes(t *testing.T) {
-	primes := SieveOfEratosthenes(10)
 	expected := []bool{false, false, true, true, false, true, false, true,
 		false, false, false}
-	if !reflect.DeepEqual(primes, expected) {
-		t.Errorf("SieveOfEratosthenes: %#v != %#v\n", primes, expected)
-	}
+	assert.Equal(t, "SieveOfEratosthenes()", expected,
+		SieveOfEratosthenes(10))
 }
 
 func TestIntsArePermutations(t *testing.T) {
@@ -128,20 +102,17 @@ func TestIntsArePermutations(t *testing.T) {
 
 func TestPrimeFactors(t *testing.T) {
 	primes := SieveOfEratosthenes(10)
-	factors := PrimeFactors(15, primes)
-	expected := []int{3, 5}
-	if !reflect.DeepEqual(expected, factors) {
-		t.Errorf("PrimeFactors: %#v != %#v\n", factors, expected)
+	tests := []struct{
+		number int
+		expected []int
+	}{
+		{15, []int{3, 5}},
+		{0, []int{}},
+		{9, []int{3, 3}},
 	}
-	factors = PrimeFactors(0, primes)
-	expected = []int{}
-	if !reflect.DeepEqual(expected, factors) {
-		t.Errorf("PrimeFactors: %#v != %#v\n", factors, expected)
-	}
-	factors = PrimeFactors(9, primes)
-	expected = []int{3, 3}
-	if !reflect.DeepEqual(expected, factors) {
-		t.Errorf("PrimeFactors: %#v != %#v\n", factors, expected)
+	for _, test := range tests {
+		assert.Equal(t, "PrimeFactors", test.expected,
+			PrimeFactors(test.number, primes))
 	}
 }
 
