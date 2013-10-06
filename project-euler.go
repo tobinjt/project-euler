e71b108da0bb0fe9acc79c31095a077c1f1c4c5f
John Tobin <johntobin@johntobin.ie>
2013-10-06 20:47:33 +0100

    Question for PE72.

--- project-euler.go
+++ project-euler.go
@@ -691,6 +691,26 @@ func projectEuler71actual(max_denominator int64) int64 {
 	return answer.Num().Int64()
 }
 
+/*
+* Consider the fraction, n/d, where n and d are positive integers. If n < d and
+* HCF(n,d)=1, it is called a reduced proper fraction.
+*
+* If we list the set of reduced proper fractions for d <= 8 in ascending order
+* of size, we get:
+*
+* 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3,
+* 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+*
+* It can be seen that there are 21 elements in this set.
+*
+* How many elements would be contained in the set of reduced proper fractions
+* for d ≤ 1,000,000?
+ */
+
+ func projectEuler72() int64 {
+	 return 0
+ }
+
 func test() int64 {
 	return int64(0)
 }
