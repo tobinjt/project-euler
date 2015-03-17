f53f5139c2e6ede3903ef95251e7786fbcce1994
John Tobin <johntobin@johntobin.ie>
2015-03-17 13:42:55 +0000

    Setup for PE81

--- project-euler.go
+++ project-euler.go
@@ -1404,3 +1404,31 @@ func projectEuler80test() int64 {
 func projectEuler80() int64 {
 	return projectEuler80actual(99)
 }
+
+/*
+* In the 5 by 5 matrix below, the minimal path sum from the top left to the
+* bottom right, by only moving to the right and down, is indicated in bold red
+* and is equal to 2427.
+*
+* 131 637 234 103  18
+* 201  96 342 965 150
+* 630 803 746 422 111
+* 537 699 497 121 956
+* 805 732 524  37 331
+*
+* Find the minimal path sum, in matrix.txt (right click and "Save Link/Target
+* As..."), a 31K text file containing a 80 by 80 matrix, from the top left to
+* the bottom right by only moving right and down.
+ */
+
+func projectEuler81actual() int64 {
+	return 0
+}
+
+func projectEuler81test() int64 {
+	return projectEuler81actual()
+}
+
+func projectEuler81() int64 {
+	return projectEuler81actual()
+}
