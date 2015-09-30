917ac585bd5cd792cc01d28dcf42071a27178d6e
John Tobin <johntobin@johntobin.ie>
2015-09-29 21:36:33 -0400

    Boilerplate for PE 92.

--- project-euler.go
+++ project-euler.go
@@ -1526,3 +1526,31 @@ func projectEuler82() int64 {
 	}
 	return projectEuler82actual(fd)
 }
+
+/*
+ * A number chain is created by continuously adding the square of the digits in
+ * a number to form a new number until it has been seen before.
+ *
+ * For example,
+ *
+ * 44 → 32 → 13 → 10 → 1 → 1
+ * 85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+ *
+ * Therefore any chain that arrives at 1 or 89 will become stuck in an endless
+ * loop. What is most amazing is that EVERY starting number will eventually
+ * arrive at 1 or 89.
+ *
+ * How many starting numbers below ten million will arrive at 89?
+ */
+
+func projectEuler92actual() int64 {
+	return 0
+}
+
+func projectEuler92test() int64 {
+	return projectEuler92actual()
+}
+
+func projectEuler92() int64 {
+	return projectEuler92actual()
+}
